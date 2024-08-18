3b11c50a6f241dc380028e584eb4802113acd48b
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-08-17 21:54:48 -0700

    Added SQLAlchemy to App 8 • Got some db functionaility setup on app too

--- App8_Django-and-bootstrap-blog-and-translator-app/app.py
+++ App8_Django-and-bootstrap-blog-and-translator-app/app.py
@@ -1,6 +1,19 @@
+# SQLAlchemy is good for establishing connections with less lines of code, based on psycopg2
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+# This is address of the databse in the computer where the app is surreounding
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/pmc-app8'
+psqldb = SQLAlchemy(app) # SQLAlchemy method to connect to db
+
+
+# Model will be object Data, inherents from the Model class from the SQLAlchemy object
+class Data(psqldb.Model):
+    __tablename__='data'
+    id=psqldb.Column(psqldb.Integer, primary_key=True)
+    emai=psqldb.Column(psqldb.String(120))
 
 # Map main homepage
 @app.route("/")
