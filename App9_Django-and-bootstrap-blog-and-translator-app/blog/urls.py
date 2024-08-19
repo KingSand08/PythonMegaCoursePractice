81da9c01afa052b6e0b8636b478f9760e319caca
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-08-18 22:35:36 -0700

    Modified home to be about page

--- App9_Django-and-bootstrap-blog-and-translator-app/blog/urls.py
+++ App9_Django-and-bootstrap-blog-and-translator-app/blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 # URL Patterns ex:(example.com/dogs/, where dogs is in slug), purpose is to connect the views and the models
 urlpatterns = [
     path('<slug:slug>', views.BlogView.as_view(), name='blog_view'), # Will look in the slug column of the db and retrieve the slug entry for each item
-    path('', views.HomeView.as_view(), name='home_view')
+    path('about/', views.AboutView.as_view(), name='about_view')
 ]
