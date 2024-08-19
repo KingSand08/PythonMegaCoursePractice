35f3b566642c19321a25d77b556e7eb7224f2c90
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-08-18 20:42:32 -0700

    Created outline of App 9 • Created env with venv (created Django in this) • Created a basic admin user account • Created a blog template (beginning template now) • Got outline of Post class done in blog -> models file     + models just started so has some errors     + This Post class is derived from the Django Models class

---
+++ App9_Django-and-bootstrap-blog-and-translator-app/blog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model): # Model is a specific class that inherits from django. It is designed to contain fields with data
+     
---
+++ App9_Django-and-bootstrap-blog-and-translator-app/mysite/__init__.py
@@ -0,0 +1,3 @@
+# Use when you want to place something immediately when webapp starts
+# Run python manage.py runserver   ->   to run the webapp
+# Run python manage.py migrate     ->   to run format the db.sqlite3 file, populating it with default tables 
